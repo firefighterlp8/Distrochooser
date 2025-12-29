c3823ec47e742167e9f6ef4cc08cc3aa2d7863a6
firefighterlp8 <linus.pfister@gmx.net>
2025-12-29 20:04:20 +0100

    Add files via upload

---
+++ Distrochooser.py
@@ -0,0 +1,136 @@
+print("Herzlich willkommen in meinem ersten Programm, dieses Programm führt dich durch einige Fragen um die vlt für dich optimale Linux Distribution zu finden, Leider hat ChatGPT bei manchen (am Ende allen) Logos 'Gekackt'")
+input("Um fortzufahren drücke bitte Enter")
+print("welche Art von Distribution bevorzugst du")
+
+print("(1) Ich möchte mein Linux von Anfang an selbst konfigurieren")
+print("(2) Ich möchte ein fertiges System installieren welches direkt benutzt werden kann")
+
+option = input("Wähle eine der beiden Optionen")
+if int(option) == 1:
+    print("bevorzugst du stabile oder schnelle Updates?")
+    print("(1) stabil")
+    print("(2) schnell")
+    option = input("Wähle eine der beiden Optionen")
+    if int(option) == 1:
+        print("Du bist mir sehr sympatisch, ich mag Debian auch sehr gerne :)")
+        print("       _,met$$$$$gg.")
+        print("    ,g$$$$$$$$$$$$$$$P.")
+        print("  ,g$$P\"\"       \"\"\"Y$$.")
+        print(" ,$$P'              `$$$.")
+        print("',$$P       ,ggs.     `$$b:")
+        print("`d$$'     ,$P\"'   .    $$$")
+        print(" $$P      d$'     ,    $$P")
+        print(" $$:      $$.   -    ,d$$'")
+        print(" $$;      Y$b._   _,d$P'")
+        print(" Y$$.    `.`\"Y$$$$P\"'")
+        print(" `$$b      \"-.__")
+        print("  `Y$$")
+        print("   `Y$$.")
+        print("     `$$b.")
+        print("       `Y$$b.")
+        print("           `\"Y$b._")
+        print("               `\"\"\"")
+
+
+    if int(option) ==2:
+        print("Es sieht so aus als wärst du ein Arch User")
+        print("                   -`")
+        print("                  .o+`")
+        print("                 `ooo/")
+        print("                `+oooo:")
+        print("               `+oooooo:")
+        print("               -+oooooo+:")
+        print("             `/:-:++oooo+:")
+        print("            `/++++/+++++++:")
+        print("           `/++++++++++++++:")
+        print("          `/+++ooooooooooooo/`")
+        print("         ./ooosssso++osssssso+`")
+        print("        .oossssso-````/ossssss+`")
+        print("       -osssssso.      :ssssssso.")
+        print("      :osssssss/        osssso+++.")
+        print("     /ossssssss/        +ssssooo/-`")
+        print("   `/ossssso+/:-        -:/+osssso+-")
+        print("  `+sso+:-`                 `.-/+oso:")
+        print(" `++:.                           `-/+/")
+        print(" .`                                 `/")
+
+
+
+
+
+if int(option) ==2:print("Du hast es also lieber bequem bei der Installation, das verstehe ich. Für dich gibt es extrem viele Distros zur Auswahl")
+print("wie oft möchtest du dein system updaten?")
+print("(1) Nie, ich möchte ein Roling Release System")
+print("(2) Etwa jedes halbe Jahr")
+print("(3) so alle 2-3 Jahre")
+option = input("Wähle eine Option")
+if int(option) == 1:
+    print("bevorzugst du pacman oder rpm?")
+    print("(1) Pacman")
+    print("(2) RPM")
+    option = input("Wähle eine Option")
+    if int(option) == 1:
+        print("Hier hast du 3 zur Auswahl, entscheide dich")
+        print("(1) Manjaro, manche behaupten es sei gut für Einsteiger, andere finden es instabiler als reines Arch")
+        print("(2) Endevour OS, im Prinzip ein reines Arch Linux das per Calamares installiert wird, im Gegensatz zu Arch aber schon einige Tools mitbringt")
+        print("(3) Cachy OS, die aktuell gehypteste Linux Distro überhaupt, durch einen optimierten Kernel richtet sich dieses System besonders an Gamer")
+        option = input("Wähle eine Option")
+        if int(option) == 1:
+            print:("Manjaro")
+            print("██████████████████  ████████")
+            print("██████████████████  ████████")
+            print("██████████████████  ████████")
+            print("██████████████████  ████████")
+            print("████████            ████████")
+            print("████████            ████████")
+            print("████████            ████████")
+            print("████████            ████████")
+            print("██████████████████  ████████")
+            print("██████████████████  ████████")
+            print("██████████████████  ████████")
+            print("██████████████████  ████████")
+
+        if int(option) == 2:
+            print("Endevour OS, mein Lieblings Arch :)")
+            print("Leider kein Logo")
+            
+        if int(option) == 3:
+            print("   ██████╗  █████╗  ██████╗ ██╗  ██╗██╗   ██╗")
+            print("  ██╔════╝ ██╔══██╗██╔════╝ ██║  ██║╚██╗ ██╔╝")
+            print("  ██║      ███████║██║      ███████║ ╚████╔╝ ")
+            print("  ██║      ██╔══██║██║      ██╔══██║  ╚██╔╝  ")
+            print("  ╚██████╗ ██║  ██║╚██████╗ ██║  ██║   ██║   ")
+            print("   ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ")
+
+
+
+
+    if int(option) == 2:
+        print("Viel Spaß mit Opensuse Tumbleweed")
+        print("          ________")
+        print("      ___/  _____ \\___")
+        print("   ___/     /     \\     \\___")
+        print("__/        |   ○   |        \\__")
+        print("\\__        |  \\___/  |        __/")
+        print("   \\___     \\_______/     ___/")
+        print("        \\___                 ___/")
+        print("             \\___       ___/")
+        print("                  \\_____/")
+
+
+
+if int(option) == 2:
+    print("legst du großen Wert auf NUR freie Software?")
+    print("(1) Ja")
+    print("(2) Nein")
+    option = input("Wähle eine Option")
+    if int(option) == 1:
+        print("Für dich wurde Fedora programmiert")
+    if int(option) == 2:
+        print("Probier doch eine STS Ausgabe von Ubuntu")
+
+if int(option) ==3:
+    print("Hier hast du viele Möglichkeiten, ich beschreibe dir 3")
+    print("(1) Linux Mint: Diese Distro hat vielen Menschen den Umstieg von Windows zu Linux vereinfacht und bietet ein extrem rundes Gesamtpaket mit einem Look der Windows 10 ähnelt")
+    print("(2) Ubuntu LTS: Dieses System wird durch die Firma Cannonical entwickelt und bietet neben einem modifiziertem Gnome-Desktop auch durch Ubuntu Pro bis zu 10 Jahre Support")
+    print("(3) Guide OS: Dieses System wurde durch die Linuxguides Community entwickelt und legt großen Wert darauf alles in der Community zu entscheiden")
